aca4428448e121376e080701dd9f9e623a7a9085
Azim Khakulov <azim@khakulov.com>
2021-03-03 13:23:53 +0500

    some tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const BACKEND_URL = "";
+
+async function getToken(username, password) {
+  const response = await fetch(`${BACKEND_URL}/api/token/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  const responseJSON = await response.json();
+  return responseJSON.token;
+}
+
+async function getBookings(token) {
+  const response = await fetch(`${BACKEND_URL}/api/booking/`, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
+
+  const responseJSON = await response.json();
+
+  return responseJSON;
+}
 
 function App() {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(async () => {
+    const token = await getToken("admin", "admin");
+    const res = await getBookings(token);
+
+    setBookings(res);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {bookings.map((booking) => (
+        <div key={booking.id}>{booking.starts_at}</div>
+      ))}
     </div>
   );
 }
